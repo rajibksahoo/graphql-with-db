fd7d26fe991c1acef2142176d94822144de52c97
Rajib Sahoo <rajibborn2win@gmail.com>
2024-09-06 12:30:16 +0530

    Application started with postgres db

---
+++ src/main/java/com/rajib/graphql/graphql_with_db/GraphqlWithDbApplication.java
@@ -0,0 +1,13 @@
+package com.rajib.graphql.graphql_with_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlWithDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlWithDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rajib/graphql/graphql_with_db/GraphqlWithDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajib.graphql.graphql_with_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlWithDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
