df30ede7952c772b86514b49878a705477b2a8eb
Rajib Sahoo <rajibborn2win@gmail.com>
2024-09-15 19:19:13 +0530

    Mapper updated

--- src/main/java/com/mmdt/graphqlapi/mapper/graphql/InputMapper.java
+++ src/main/java/com/mmdt/graphqlapi/mapper/graphql/InputMapper.java
@@ -3,10 +3,12 @@ package com.mmdt.graphqlapi.mapper.graphql;
 
 import com.mmdt.graphqlapi.dtos.*;
 import com.mmdt.graphqlapi.entities.*;
+import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring",
+        builder =@Builder(disableBuilder = true))
 public interface InputMapper {
     @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "updatedAt", ignore = true)
@@ -16,15 +18,15 @@ public interface InputMapper {
     @Mapping(target = "id", ignore = true)
     User userToUser(User user);
 
-    //@Mapping(target = "createdAt", ignore = true)
-    //@Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
     Meal toEntity(MealInput mealInput );
 
-    //@Mapping(target = "createdAt", ignore = true)
-    //@Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
     MealPlan toEntity(MealPlanInput mealPlanInput);
 
-    //@Mapping(target = "id", ignore = true)
+    @Mapping(target = "id", ignore = true)
     Instruction toEntity(InstructionInput instructionInput);
 
     Ingredient toEntity(IngredientInput ingredientInput);
